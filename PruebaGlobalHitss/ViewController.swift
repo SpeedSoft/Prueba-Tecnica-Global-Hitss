1a86e4fa27eb978821eb6fa061c585f84ebfa396
bcapetillo <bcapetillo.dv@gmail.com>
2021-09-14 21:57:25 -0500

    Initial Commit

---
+++ PruebaGlobalHitss/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PruebaGlobalHitss
+//
+//  Created by Bart on 14/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
